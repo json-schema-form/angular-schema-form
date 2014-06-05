0bc2c6ba93293b4fe129616c6b97e0171bec056b
David Jensen <david.lgj@gmail.com>
2014-06-05 15:50:04 +0200

    Empty and pristine fields are no succesful
    Even if they validate (i.e are not required)

--- src/directives/schema-validate.js
+++ src/directives/schema-validate.js
@@ -58,12 +58,13 @@ angular.module('schemaForm').directive('schemaValidate',function(){
       });
 
       //This works since we now we're inside a decorator and that this is the decorators scope.
+      //If $pristine and empty don't show success (even if it's valid)
       scope.hasSuccess = function(){
-        return scope.ngModel.$valid && !scope.ngModel.$pristine;
+        return ngModel.$valid && (!ngModel.$pristine || !ngModel.$isEmpty(ngModel.$modelValue));
       };
-      
+
       scope.hasError = function(){
-        return scope.ngModel.$invalid && !scope.ngModel.$pristine;
+        return ngModel.$invalid && !ngModel.$pristine;
       };
 
       scope.schemaError = function() {
