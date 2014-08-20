e7ff79aabd73d731276ef7a84022de5d8f4355cc
David Jensen <david.lgj@gmail.com>
2014-08-20 12:55:02 +0200

    Updated minified files

--- dist/schema-form.js
+++ dist/schema-form.js
@@ -1,3 +1,6 @@
+// Deps is sort of a problem for us, maybe in the future we will ask the user to depend
+// on modules for add-ons
+
 var deps = ['ObjectPath'];
 try {
   //This throws an expection if module does not exist.
@@ -9,6 +12,12 @@ try {
   //This throws an expection if module does not exist.
   angular.module('ui.sortable');
   deps.push('ui.sortable');
+} catch (e) {}
+
+try {
+  //This throws an expection if module does not exist.
+  angular.module('angularSpectrumColorpicker');
+  deps.push('angularSpectrumColorpicker');
 } catch (e) {}
 
 angular.module('schemaForm', deps);
