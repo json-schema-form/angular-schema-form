8e4cba91578cc9a78a12f56c036d1c167884819c
Cameron Edwards <cameronprattedwards@gmail.com>
2014-09-09 13:18:44 -0400

    Move bootstrap task into gulp/tasks

--- gulpfile.js
+++ gulpfile.js
@@ -10,29 +10,7 @@ var streamqueue = require('streamqueue');
 var jscs = require('gulp-jscs');
 
 
-
-gulp.task('bootstrap', function() {
-  var stream = streamqueue({objectMode: true});
-  stream.queue(
-              gulp.src('./src/directives/decorators/bootstrap/*.html')
-                  .pipe(minifyHtml({
-                    empty: true,
-                    spare: true,
-                    quotes: true
-                  }))
-                  .pipe(templateCache({
-                    module: 'schemaForm',
-                    root: 'directives/decorators/bootstrap/'
-                  }))
-    );
-  stream.queue(gulp.src('./src/directives/decorators/bootstrap/*.js'));
-
-  stream.done()
-        .pipe(concat('bootstrap-decorator.min.js'))
-        .pipe(uglify())
-        .pipe(gulp.dest('./dist/'));
-
-});
+require('./gulp');
 
 gulp.task('bootstrap-datepicker', function() {
   var stream = streamqueue({objectMode: true});
