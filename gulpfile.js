e936ef7d03b303a27df4be120b1207d4072ef834
Cameron Edwards <cameronprattedwards@gmail.com>
2014-09-09 13:24:07 -0400

    Move minify task into gulp/tasks

--- gulpfile.js
+++ gulpfile.js
@@ -12,18 +12,6 @@ var jscs = require('gulp-jscs');
 
 require('./gulp');
 
-gulp.task('minify', function() {
-  gulp.src([
-    './src/module.js',
-    './src/sfPath.js',
-    './src/services/*.js',
-    './src/directives/*.js'
-  ])
-  .pipe(concat('schema-form.min.js'))
-  .pipe(uglify())
-  .pipe(gulp.dest('./dist/'));
-});
-
 gulp.task('non-minified-dist', function() {
   gulp.src([
     './src/module.js',
