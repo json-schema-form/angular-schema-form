fcf4ca44c9f6c495c95743d768323373b0b1845b
Cameron Edwards <cameronprattedwards@gmail.com>
2014-09-09 13:37:21 -0400

    Fix indentation in entry file

--- gulp/index.js
+++ gulp/index.js
@@ -1,7 +1,7 @@
 var fs = require('fs'),
-	tasks = fs.readdirSync('./gulp/tasks'),
-	gulp = require('gulp');
+  tasks = fs.readdirSync('./gulp/tasks'),
+  gulp = require('gulp');
 
 tasks.forEach(function(task) {
-	require('./tasks/' + task);
+  require('./tasks/' + task);
 });
