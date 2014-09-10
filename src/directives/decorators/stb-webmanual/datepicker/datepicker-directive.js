b9c3422d45973f2dc95314e752404477227ecf8c
Savchenko, Anton <anton.savchenko@storebrand.no>
2014-09-10 19:50:28 +0300

    fixed datepicker directive

--- src/directives/decorators/stb-webmanual/datepicker/datepicker-directive.js
+++ src/directives/decorators/stb-webmanual/datepicker/datepicker-directive.js
@@ -3,18 +3,16 @@ angular.module('schemaForm').directive('datepicker', function() {
     restrict: 'A',
     require : 'ngModel',
     link : function (scope, element, attrs) {
-      $(function() {
-        /* Unit test*/
-        if (!element.datepicker) {
-          return;
-        }
-        element.datepicker({
-          dateFormat:'dd/mm/yy',
-          setDefaults: $.datepicker.regional.no,
-          prevText: '<',
-          nextText: '>'
-        }).datepicker('setDate', new Date());
-      });
+      /* Unit test*/
+      if (!$.datepicker) {
+        return;
+      }
+      $(element).datepicker({
+        dateFormat:'dd/mm/yy',
+        setDefaults: $.datepicker.regional.no,
+        prevText: '<',
+        nextText: '>'
+      }).datepicker('setDate', new Date());
 
     }
   };
