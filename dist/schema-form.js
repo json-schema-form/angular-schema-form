acc5cecabf4200806e639f3b6ff183bc3a6f7728
Savchenko, Anton <anton.savchenko@storebrand.no>
2014-10-14 10:20:56 +0300

    add ability to validate on blur CSI-159

--- dist/schema-form.js
+++ dist/schema-form.js
@@ -1319,6 +1319,24 @@ angular.module('schemaForm').directive('sfChanged', function() {
   };
 });
 
+// override the default input to update on blur
+angular.module('schemaForm').directive('ngModelOnblur', function() {
+  return {
+    restrict: 'A',
+    require: 'ngModel',
+    priority: 1, // needed for angular 1.2.x
+    link: function(scope, elm, attr, ngModelCtrl) {
+      if (attr.type === 'radio' || attr.type === 'checkbox') return;
+
+      elm.unbind('input').unbind('keydown').unbind('change');
+      elm.bind('blur', function() {
+        scope.$apply(function() {
+          ngModelCtrl.$setViewValue(elm.val());
+        });
+      });
+    }
+  };
+});
 /*
 FIXME: real documentation
 <form sf-form="form"  sf-schema="schema" sf-decorator="foobar"></form>
---
+++ src/directives/modelOnBlur.js
@@ -0,0 +1,18 @@
+// override the default input to update on blur
+angular.module('schemaForm').directive('ngModelOnblur', function() {
+  return {
+    restrict: 'A',
+    require: 'ngModel',
+    priority: 1, // needed for angular 1.2.x
+    link: function(scope, elm, attr, ngModelCtrl) {
+      if (attr.type === 'radio' || attr.type === 'checkbox') return;
+
+      elm.unbind('input').unbind('keydown').unbind('change');
+      elm.bind('blur', function() {
+        scope.$apply(function() {
+          ngModelCtrl.$setViewValue(elm.val());
+        });
+      });
+    }
+  };
+});
