1ad015ac9429fab058628f81478b0bd92dbc7f6a
sebastien_andreu <deadyman@free.fr>
2014-10-23 14:17:33 +0000

    Redraw form on schema, form or option change

--- test/directives/schema-form-test.js
+++ test/directives/schema-form-test.js
@@ -173,6 +173,59 @@ describe('directive',function(){
     });
   });
 
+  it('should regenerate html on schema update',function(){
+
+    inject(function($compile,$rootScope){
+      var scope = $rootScope.$new();
+      scope.person = {};
+
+      scope.schema = angular.copy(exampleSchema);
+
+      scope.form = ["*"];
+
+      var tmpl = angular.element('<form sf-schema="schema" sf-form="form" sf-model="person"></form>');
+
+      $compile(tmpl)(scope);
+      $rootScope.$apply();
+      
+      tmpl.children().eq(0).children().eq(0).find('label').text().should.be.equal('Name');
+      
+      scope.schema.properties.name.title = 'Updated Name';
+      $rootScope.$apply();
+
+      tmpl.children().eq(0).children().eq(0).find('label').text().should.be.equal('Updated Name');
+
+    });
+  });
+
+  it('should regenerate html on form update',function(){
+
+    inject(function($compile,$rootScope){
+      var scope = $rootScope.$new();
+      scope.person = {};
+
+      scope.schema = angular.copy(exampleSchema);
+
+      scope.form = [{
+        key: 'name',
+        title: 'Form Name'
+      }];
+
+      var tmpl = angular.element('<form sf-schema="schema" sf-form="form" sf-model="person"></form>');
+
+      $compile(tmpl)(scope);
+      $rootScope.$apply();
+      
+      tmpl.children().eq(0).children().eq(0).find('label').text().should.be.equal('Form Name');
+      
+      scope.form[0].title = 'Updated Name';
+      $rootScope.$apply();
+
+      tmpl.children().eq(0).children().eq(0).find('label').text().should.be.equal('Updated Name');
+
+    });
+  });
+
   it('should preserve existing html and insert fields in matching slots',function(){
 
     inject(function($compile,$rootScope){
