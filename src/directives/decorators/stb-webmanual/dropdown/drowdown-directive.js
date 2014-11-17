732fffd66c20479bf9eede9c518f0a5ef10ef9bc
Savchenko, Anton <anton.savchenko@storebrand.no>
2014-11-17 17:22:21 +0200

    update styles for storebrand.ny 0.45

--- src/directives/decorators/stb-webmanual/dropdown/drowdown-directive.js
+++ src/directives/decorators/stb-webmanual/dropdown/drowdown-directive.js
@@ -3,8 +3,14 @@ angular.module('schemaForm').directive('stbDropdown', ['$timeout', function($tim
     restrict: 'A',
     require : 'ngModel',
     link : function (scope, element, attrs, ngModelCtrl) {
-      $timeout(function () {
-        $(element).chosen();})
+      scope.$watch(function () {
+        var el = $(element);
+        return el.css('display') === 'none' || el.width() === 0;
+      }, function (value) {
+        if (!value) {
+          $(element).chosen();
+        }
+      });
     }
   };
 }]);
