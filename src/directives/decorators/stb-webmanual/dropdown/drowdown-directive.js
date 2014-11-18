eee98a7e188d1a05900482f6d6a9dd7c6f89b214
Savchenko, Anton <anton.savchenko@storebrand.no>
2014-11-18 13:40:25 +0200

    fix setting view value of dropdown

--- src/directives/decorators/stb-webmanual/dropdown/drowdown-directive.js
+++ src/directives/decorators/stb-webmanual/dropdown/drowdown-directive.js
@@ -1,4 +1,4 @@
-angular.module('schemaForm').directive('stbDropdown', ['$timeout', function($timeout) {
+angular.module('schemaForm').directive('stbDropdown', ['$timeout', '$parse', function($timeout, $parse) {
   return {
     restrict: 'A',
     require : 'ngModel',
@@ -8,7 +8,14 @@ angular.module('schemaForm').directive('stbDropdown', ['$timeout', function($tim
         return el.css('display') === 'none' || el.width() === 0;
       }, function (value) {
         if (!value) {
-          $(element).chosen();
+          $(element).chosen().unbind('change').change(function (e, selected) {
+            $.each(scope.form.titleMap, function (index, item) {
+                if (item.name === selected.selected) {
+
+                  ngModelCtrl.$setViewValue({name: item.name, value: item.value});
+                }
+            })
+          });
         }
       });
     }
