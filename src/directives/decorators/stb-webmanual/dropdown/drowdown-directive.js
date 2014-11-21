46019289d97b829f3511de4c9e66d8b4540fe5c9
Savchenko, Anton <anton.savchenko@storebrand.no>
2014-11-21 17:48:26 +0200

    change the way of unbinding the listener

--- src/directives/decorators/stb-webmanual/dropdown/drowdown-directive.js
+++ src/directives/decorators/stb-webmanual/dropdown/drowdown-directive.js
@@ -3,19 +3,20 @@ angular.module('schemaForm').directive('stbDropdown', ['$timeout', '$parse', fun
     restrict: 'A',
     require : 'ngModel',
     link : function (scope, element, attrs, ngModelCtrl) {
+      var listener = function (e, selected) {
+        $.each(scope.form.titleMap, function (index, item) {
+          if (selected && item.name === selected.selected) {
+
+            ngModelCtrl.$setViewValue({name: item.name, value: item.value});
+          }
+        })
+      };
       scope.$watch(function () {
         var el = $(element);
         return el.css('display') === 'none' || el.width() === 0;
       }, function (value) {
         if (!value) {
-          $(element).chosen().unbind('change').change(function (e, selected) {
-            $.each(scope.form.titleMap, function (index, item) {
-                if (selected && item.name === selected.selected) {
-
-                  ngModelCtrl.$setViewValue({name: item.name, value: item.value});
-                }
-            })
-          });
+          $(element).chosen().unbind('change', listener).change(listener);
         }
       });
     }
