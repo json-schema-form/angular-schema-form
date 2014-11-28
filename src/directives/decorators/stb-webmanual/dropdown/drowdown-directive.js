ca1de53f0d0cdbe8ac39a973ed3e76492ea5e9d7
Savchenko, Anton <anton.savchenko@storebrand.no>
2014-11-28 19:00:06 +0200

    update whiteboxes. I'm tired with it

--- src/directives/decorators/stb-webmanual/dropdown/drowdown-directive.js
+++ src/directives/decorators/stb-webmanual/dropdown/drowdown-directive.js
@@ -13,7 +13,7 @@ angular.module('schemaForm').directive('stbDropdown', ['$timeout', '$parse', fun
       };
       scope.$watch(function () {
         var el = $(element);
-        return el.css('display') === 'none' || el.width() === 0;
+        return el.css('display') === 'none';
       }, function (value) {
         if (!value) {
           $(element).chosen().unbind('change', listener).change(listener);
