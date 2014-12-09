8612be2b7460181db12fa4c84853b51e933c22b6
Savchenko, Anton <anton.savchenko@storebrand.no>
2014-12-09 14:17:02 +0200

    fixed maxLength property

--- dist/schema-form.js
+++ dist/schema-form.js
@@ -1536,18 +1536,15 @@ angular.module('schemaForm')
     }]);
 
 // override the default input to update on blur
-angular.module('schemaForm').directive('ngModelOnblur', ['formFormatters', function(formFormatters) {
+angular.module('schemaForm').directive('ngModelOnblur', ['formFormatters', '$parse', function(formFormatters, $parse) {
   return {
     restrict: 'A',
     require: 'ngModel',
-    scope: {
-      formatterName: "="
-    },
     priority: 1, // needed for angular 1.2.x
     link: function(scope, elm, attr, ngModelCtrl) {
       if (attr.type === 'radio' || attr.type === 'checkbox') return;
 
-      var formatter = formFormatters.getFormatter(scope.formatterName) || function (input) {
+      var formatter = formFormatters.getFormatter(scope.$eval(attr.formatterName)) || function (input) {
           return input;
       };
 
@@ -1563,6 +1560,7 @@ angular.module('schemaForm').directive('ngModelOnblur', ['formFormatters', funct
       });
 
       var maxLength = scope.$eval(attr.modelMaxLength);
+
 
       if (angular.isDefined(maxLength)) {
 
--- src/directives/decorators/stb-webmanual/datepicker/datepicker-directive.js
+++ src/directives/decorators/stb-webmanual/datepicker/datepicker-directive.js
@@ -15,13 +15,13 @@ angular.module('schemaForm').directive('stbDatepicker', ['$timeout', function($t
 
       $(element).parent().datetimepicker({
         pickTime: false,
-        language: 'nb',
+        language: 'nn',
         format: "DD.MM.YYYY",
         minDate: scope.$eval(attrs.minDate) || scope.$eval(attrs.disableUntilToday) && today.toDate(),
         maxDate: scope.$eval(attrs.maxDate) || difference && moment(today).add(difference, 'Month').toDate()
     }).on('dp.change', function (e) {
         scope.$apply(function () {
-          ngModelCtrl.$setViewValue(e.date.toISOString())
+          ngModelCtrl.$setViewValue(e.date.toISOString());
         });
       }).on('dp.error', function (e) {
         scope.$apply(function () {
