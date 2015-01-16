daf28b918906ef41804a226ef9c1262025d82cde
Navka, Kate <kateryne.navka@storebrand.no>
2015-01-16 08:48:33 +0100

    BI-2160: add button directive

--- src/directives/decorators/stb-webmanual/stb-webmanual-decorator.js
+++ src/directives/decorators/stb-webmanual/stb-webmanual-decorator.js
@@ -30,6 +30,7 @@ angular.module('schemaForm').config(['schemaFormDecoratorsProvider', function(de
     successbox: base + 'successbox.html',
     errorbox: base + 'errorbox.html',
     infodate: base + 'date-info.html',
+    button: base + 'button.html',
     standarttable: base + 'standart-table.html',
     whitebox: base + 'white-box.html'
   }, [
@@ -59,6 +60,7 @@ angular.module('schemaForm').config(['schemaFormDecoratorsProvider', function(de
     radios: base + 'radios.html',
     'radios-inline': base + 'radios-inline.html',
     radiobuttons: base + 'radio-buttons.html',
+    button: base + 'button.html',
     standarttable: base + 'standart-table.html',
     whitebox: base + 'white-box.html',
     steps: base + 'steps.html',
