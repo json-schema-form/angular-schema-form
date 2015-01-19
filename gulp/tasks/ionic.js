1e4d732028fb1ab2440fcda679fa26f309669d7a
Javi Fernández <jfernandezriverola@gmail.com>
2015-01-19 19:40:38 +0100

    ionic decorator added

---
+++ gulp/tasks/ionic.js
@@ -0,0 +1,29 @@
+var gulp = require('gulp'),
+  streamqueue = require('streamqueue'),
+  minifyHtml = require('gulp-minify-html'),
+  templateCache = require('gulp-angular-templatecache'),
+  concat = require('gulp-concat'),
+  uglify = require('gulp-uglify');
+
+gulp.task('ionic', function() {
+  var stream = streamqueue({objectMode: true});
+  stream.queue(
+    gulp.src('./src/directives/decorators/ionic/*.html')
+    .pipe(minifyHtml({
+      empty: true,
+      spare: true,
+      quotes: true
+    }))
+    .pipe(templateCache({
+      module: 'schemaForm',
+      root: 'directives/decorators/ionic/'
+    }))
+    );
+  stream.queue(gulp.src('./src/directives/decorators/ionic/*.js'));
+
+  stream.done()
+  .pipe(concat('ionic-decorator.min.js'))
+  .pipe(uglify())
+  .pipe(gulp.dest('./dist/'));
+
+});
---
+++ src/directives/decorators/ionic/ionic-decorator.js
@@ -0,0 +1,61 @@
+angular.module('schemaForm').config(['schemaFormDecoratorsProvider', function(decoratorsProvider) {
+  var base = 'directives/decorators/ionic/';
+
+  decoratorsProvider.createDecorator('ionicDecorator', {
+    textarea: base + 'textarea.html',
+    fieldset: base + 'fieldset.html',
+    array: base + 'array.html',
+    tabarray: base + 'tabarray.html',
+    tabs: base + 'tabs.html',
+    section: base + 'section.html',
+    conditional: base + 'section.html',
+    actions: base + 'actions.html',
+    select: base + 'select.html',
+    checkbox: base + 'checkbox.html',
+    checkboxes: base + 'checkboxes.html',
+    number: base + 'default.html',
+    password: base + 'default.html',
+    submit: base + 'submit.html',
+    button: base + 'submit.html',
+    radios: base + 'radios.html',
+    'radios-inline': base + 'radios-inline.html',
+    radiobuttons: base + 'radio-buttons.html',
+    help: base + 'help.html',
+    'default': base + 'default.html'
+  }, [
+    // function(form) {
+    //   if (form.readonly && form.key && form.type !== 'fieldset') {
+    //     return base + 'readonly.html';
+    //   }
+    // }
+  ]);
+
+  //manual use directives
+  decoratorsProvider.createDirectives({
+    textarea: base + 'textarea.html',
+    select: base + 'select.html',
+    checkbox: base + 'checkbox.html',
+    checkboxes: base + 'checkboxes.html',
+    number: base + 'default.html',
+    submit: base + 'submit.html',
+    button: base + 'submit.html',
+    text: base + 'default.html',
+    date: base + 'default.html',
+    password: base + 'default.html',
+    datepicker: base + 'datepicker.html',
+    input: base + 'default.html',
+    radios: base + 'radios.html',
+    'radios-inline': base + 'radios-inline.html',
+    radiobuttons: base + 'radio-buttons.html',
+  });
+
+}]).directive('sfFieldset', function() {
+  return {
+    transclude: true,
+    scope: true,
+    templateUrl: 'directives/decorators/ionic/fieldset-trcl.html',
+    link: function(scope, element, attrs) {
+      scope.title = scope.$eval(attrs.title);
+    }
+  };
+});
