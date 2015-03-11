1dfa6c3bdf061e1b3824ef137a03c500c3a0ad48
ulion <ulion2002@gmail.com>
2015-03-11 08:30:34 +0800

    Enhance the select field with null value support

--- src/services/schema-form.js
+++ src/services/schema-form.js
@@ -359,6 +359,21 @@ angular.module('schemaForm').provider('schemaForm',
           }
         }
 
+        if (obj.type === 'select' && obj.titleMap && obj.allowAutoNullOption !== false) {
+          // when allowNull === false, we leave titleMap as it is.
+          // else we filter out the null value and setup allowNull to the label
+          // so the select template will behaviour different according to it.
+          obj.titleMap = obj.titleMap.filter(function(item) {
+            if (item.value === null) {
+              // so we have null value option, setup the label and make it always selectable.
+              if (obj.allowAutoNullOption === undefined)
+                obj.allowAutoNullOption = item.name === null ? '' : item.name;
+              return false;
+            }
+            return true;
+          });
+        }
+
         // Are we inheriting readonly?
         if (readonly === true) { // Inheriting false is not cool.
           obj.readonly = true;
