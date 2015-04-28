7b53c63625003ebc735471b60cdefe9e37f1b371
jbsaff <james.saffell@spr.com>
2015-04-28 12:46:36 -0500

    Revert "Extended schema-validate directive to handle cleaning the model when a form field element triggers the $destroy. Uses a new service, based on Select, to traverse the model and update it to the value chosen as part of the configured destroyStrategy. This destroyStrategy can be configured at the field, or as part of the forms global options. If both are defined, the field-level strategy will override."
    This reverts commit a31cdbeb56085c24783b8ae696422292661b4062.

--- src/directives/schema-validate.js
+++ src/directives/schema-validate.js
@@ -1,6 +1,4 @@
-angular.module('schemaForm').directive('schemaValidate', ['sfValidator', 'sfSelect', 'sfUnselect',
-  function(sfValidator, sfSelect, sfUnselect) {
-
+angular.module('schemaForm').directive('schemaValidate', ['sfValidator', 'sfSelect', function(sfValidator, sfSelect) {
   return {
     restrict: 'A',
     scope: false,
@@ -9,6 +7,7 @@ angular.module('schemaForm').directive('schemaValidate', ['sfValidator', 'sfSele
     priority: 500,
     require: 'ngModel',
     link: function(scope, element, attrs, ngModel) {
+
 
       // We need the ngModelController on several places,
       // most notably for errors.
@@ -101,68 +100,6 @@ angular.module('schemaForm').directive('schemaValidate', ['sfValidator', 'sfSele
         }
 
       });
-
-
-      var DEFAULT_DESTROY_STRATEGY;
-      if (scope.options && scope.options.formDefaults) {
-        var formDefaultDestroyStrategy = scope.options.formDefaults.destroyStrategy;
-        var isValidFormDefaultDestroyStrategy = (formDefaultDestroyStrategy === undefined ||
-        formDefaultDestroyStrategy === '' ||
-        formDefaultDestroyStrategy === null ||
-        formDefaultDestroyStrategy === 'retain');
-        if (isValidFormDefaultDestroyStrategy) {
-          DEFAULT_DESTROY_STRATEGY = formDefaultDestroyStrategy;
-        }
-        else {
-          console.warn('Unrecognized formDefaults.destroyStrategy: \'%s\'. Used undefined instead.',
-              formDefaultDestroyStrategy);
-          DEFAULT_DESTROY_STRATEGY = undefined;
-        }
-      }
-
-      // Clean up the model when the corresponding form field is $destroy-ed.
-      // Default behavior can be supplied as a formDefault, and behavior can be overridden in the form definition.
-      scope.$on('$destroy', function() {
-        var form = getForm();
-        var destroyStrategy = form.destroyStrategy; // Either set in form definition, or as part of formDefaults.
-        var schemaType = getSchemaType();
-
-        if (destroyStrategy && destroyStrategy !== 'retain' ) {
-          // Don't recognize the strategy, so give a warning.
-          console.warn('Unrecognized destroyStrategy: \'%s\'. Used default instead.', destroyStrategy);
-          destroyStrategy = DEFAULT_DESTROY_STRATEGY;
-        }
-        else if (schemaType !== 'string' && destroyStrategy === '') {
-          // Only 'string' type fields can have an empty string value as a valid option.
-          console.warn('Attempted to use empty string destroyStrategy on non-string form type. Used default instead.');
-          destroyStrategy = DEFAULT_DESTROY_STRATEGY;
-        }
-
-        if (destroyStrategy === 'retain') {
-          return; // Valid option to avoid destroying data in the model.
-        }
-
-        destroyUsingStrategy(destroyStrategy);
-
-        function destroyUsingStrategy(strategy) {
-          var strategyIsDefined = (strategy === null || strategy === '' || typeof strategy == undefined);
-          if (!strategyIsDefined){
-            strategy = DEFAULT_DESTROY_STRATEGY;
-          }
-          sfUnselect(scope.form.key, scope.model, strategy);
-        }
-
-        function getSchemaType() {
-          if (form.schema) {
-            schemaType = form.schema.type;
-          }
-          else {
-            schemaType = null;
-          }
-        }
-      });
-
-
 
       scope.schemaError = function() {
         return error;
