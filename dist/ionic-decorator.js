b2ae32fc6da44f4b897847d3e3b9e2ac770b9923
Javi Fernández <jfernandezriverola@gmail.com>
2015-04-28 15:25:22 +0200

    more

--- dist/ionic-decorator.js
+++ dist/ionic-decorator.js
@@ -78,13 +78,15 @@ angular.module('schemaForm').config(['schemaFormDecoratorsProvider', function(de
       scope.title = scope.$eval(attrs.title);
     }
   };
-}).directive('formatDate',function (){
-    return {
-        require: 'ngModel',
-        link: function(scope, elem, attr, modelCtrl) {
-            modelCtrl.$formatters.push(function(modelValue){
-                return new Date(modelValue);
-            });
-        }
-    };
 });
+
+// .directive('formatDate',function (){
+//     return {
+//         require: 'ngModel',
+//         link: function(scope, elem, attr, modelCtrl) {
+//             modelCtrl.$formatters.push(function(modelValue){
+//                 return new Date(modelValue);
+//             });
+//         }
+//     };
+// });
