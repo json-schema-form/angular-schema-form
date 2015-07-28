a52691f0a1f1f24c0c0d99533837322b7c201432
Angela Haralson <angela.haralson@greenskycredit.com>
2015-07-28 14:34:39 -0400

    Pulled the schemaForm module definition to the top and removed dependencies

--- gulp/tasks/minify.js
+++ gulp/tasks/minify.js
@@ -1,28 +1,34 @@
 var gulp = require('gulp'),
+  streamqueue = require('streamqueue'),
   concat = require('gulp-concat'),
   rename = require('gulp-rename'),
   umd    = require('gulp-umd'),
   uglify = require('gulp-uglify');
 
 gulp.task('minify', function() {
-  gulp.src([
-    './src/module.js',
-    './src/sfPath.js',
-    './src/services/*.js',
-    './src/directives/*.js'
-    ])
+  var stream = streamqueue({objectMode: true});
+  stream.queue(gulp.src('./src/module.js'));
+  stream.queue(
+    gulp.src([
+      './src/sfPath.js',
+      './src/services/*.js',
+      './src/directives/*.js'
+      ])
+    .pipe(umd({
+      dependencies: function() {
+        return [
+          {name: 'angular'},
+          {name: 'objectpath'},
+          {name: 'tv4'},
+        ];
+      },
+      exports: function() {return 'schemaForm';},
+      namespace: function() {return 'schemaForm';}
+      }))
+  );
+
+  stream.done()
   .pipe(concat('schema-form.js'))
-  .pipe(umd({
-    dependencies: function() {
-      return [
-        {name: 'angular'},
-        {name: 'objectpath'},
-        {name: 'tv4'},
-      ];
-    },
-    exports: function() {return 'schemaForm';},
-    namespace: function() {return 'schemaForm';}
-    }))
   .pipe(gulp.dest('./dist/'))
   .pipe(uglify())
   .pipe(rename('schema-form.min.js'))
