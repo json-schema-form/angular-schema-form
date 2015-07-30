9b1214ef56da649668ecd5ba146fd761247d01f5
Angela Haralson <angela.haralson@greenskycredit.com>
2015-07-30 12:38:48 -0400

    Revert 4c8f1f4..d1082d5
    This rolls back to commit 4c8f1f4f9a6193d8fe4d9d26b836f89040f5cfd5.

--- gulp/tasks/minify.js
+++ gulp/tasks/minify.js
@@ -1,34 +1,28 @@
 var gulp = require('gulp'),
-  streamqueue = require('streamqueue'),
   concat = require('gulp-concat'),
   rename = require('gulp-rename'),
   umd    = require('gulp-umd'),
   uglify = require('gulp-uglify');
 
 gulp.task('minify', function() {
-  var stream = streamqueue({objectMode: true});
-  stream.queue(gulp.src('./src/module.js'));
-  stream.queue(
-    gulp.src([
-      './src/sfPath.js',
-      './src/services/*.js',
-      './src/directives/*.js'
-      ])
-    .pipe(umd({
-      dependencies: function() {
-        return [
-          {name: 'angular'},
-          {name: 'objectpath'},
-          {name: 'tv4'},
-        ];
-      },
-      exports: function() {return 'schemaForm';},
-      namespace: function() {return 'schemaForm';}
-      }))
-  );
-
-  stream.done()
+  gulp.src([
+    './src/module.js',
+    './src/sfPath.js',
+    './src/services/*.js',
+    './src/directives/*.js'
+    ])
   .pipe(concat('schema-form.js'))
+  .pipe(umd({
+    dependencies: function() {
+      return [
+        {name: 'angular'},
+        {name: 'objectpath'},
+        {name: 'tv4'},
+      ];
+    },
+    exports: function() {return 'schemaForm';},
+    namespace: function() {return 'schemaForm';}
+    }))
   .pipe(gulp.dest('./dist/'))
   .pipe(uglify())
   .pipe(rename('schema-form.min.js'))
--- src/module.js
+++ src/module.js
@@ -1,2 +1,23 @@
+// Deps is sort of a problem for us, maybe in the future we will ask the user to depend
+// on modules for add-ons
 
-var schemaForm = angular.module('schemaForm', ['ngSanitize']);
+var deps = [];
+try {
+  //This throws an expection if module does not exist.
+  angular.module('ngSanitize');
+  deps.push('ngSanitize');
+} catch (e) {}
+
+try {
+  //This throws an expection if module does not exist.
+  angular.module('ui.sortable');
+  deps.push('ui.sortable');
+} catch (e) {}
+
+try {
+  //This throws an expection if module does not exist.
+  angular.module('angularSpectrumColorpicker');
+  deps.push('angularSpectrumColorpicker');
+} catch (e) {}
+
+var schemaForm = angular.module('schemaForm', deps);
