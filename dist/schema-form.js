70d55b65cf0a86b58e9c1da5ddad4a33e48e5387
Marcel <iamanthropic@gmail.com>
2016-04-04 00:09:06 +1000

    Release 0.8.13
    Maintenance release 0.8.13
    Bugfix copy the initial form default on form redraw @stanimoto
    Bugfix for revalidating the model after custom validation event is
    broadcast @cepauskas
    New event value. The form name is now passed in validate and error
    events @tomsowerby & @LeonardoGentile

--- dist/schema-form.js
+++ dist/schema-form.js
@@ -65,81 +65,6 @@ angular.module('schemaForm').provider('sfPath',
   };
 }]);
 
-/**
- * @ngdoc service
- * @name sfSelect
- * @kind function
- *
- */
-angular.module('schemaForm').factory('sfSelect', ['sfPath', function(sfPath) {
-  var numRe = /^\d+$/;
-
-  /**
-    * @description
-    * Utility method to access deep properties without
-    * throwing errors when things are not defined.
-    * Can also set a value in a deep structure, creating objects when missing
-    * ex.
-    * var foo = Select('address.contact.name',obj)
-    * Select('address.contact.name',obj,'Leeroy')
-    *
-    * @param {string} projection A dot path to the property you want to get/set
-    * @param {object} obj   (optional) The object to project on, defaults to 'this'
-    * @param {Any}    valueToSet (opional)  The value to set, if parts of the path of
-    *                 the projection is missing empty objects will be created.
-    * @returns {Any|undefined} returns the value at the end of the projection path
-    *                          or undefined if there is none.
-    */
-  return function(projection, obj, valueToSet) {
-    if (!obj) {
-      obj = this;
-    }
-    //Support [] array syntax
-    var parts = typeof projection === 'string' ? sfPath.parse(projection) : projection;
-
-    if (typeof valueToSet !== 'undefined' && parts.length === 1) {
-      //special case, just setting one variable
-      obj[parts[0]] = valueToSet;
-      return obj;
-    }
-
-    if (typeof valueToSet !== 'undefined' &&
-        typeof obj[parts[0]] === 'undefined') {
-       // We need to look ahead to check if array is appropriate
-      obj[parts[0]] = parts.length > 2 && numRe.test(parts[1]) ? [] : {};
-    }
-
-    var value = obj[parts[0]];
-    for (var i = 1; i < parts.length; i++) {
-      // Special case: We allow JSON Form syntax for arrays using empty brackets
-      // These will of course not work here so we exit if they are found.
-      if (parts[i] === '') {
-        return undefined;
-      }
-      if (typeof valueToSet !== 'undefined') {
-        if (i === parts.length - 1) {
-          //last step. Let's set the value
-          value[parts[i]] = valueToSet;
-          return valueToSet;
-        } else {
-          // Make sure to create new objects on the way if they are not there.
-          // We need to look ahead to check if array is appropriate
-          var tmp = value[parts[i]];
-          if (typeof tmp === 'undefined' || tmp === null) {
-            tmp = numRe.test(parts[i + 1]) ? [] : {};
-            value[parts[i]] = tmp;
-          }
-          value = tmp;
-        }
-      } else if (value) {
-        //Just get nex value.
-        value = value[parts[i]];
-      }
-    }
-    return value;
-  };
-}]);
-
 
 // FIXME: type template (using custom builder)
 angular.module('schemaForm').provider('sfBuilder', ['sfPathProvider', function(sfPathProvider) {
@@ -682,6 +607,9 @@ angular.module('schemaForm').provider('schemaFormDecorators',
                         scope.ngModel.$setValidity(error, validity === true);
 
                         if (validity === true) {
+                          // Re-trigger model validator, that model itself would be re-validated
+                          scope.ngModel.$validate();
+
                           // Setting or removing a validity can change the field to believe its valid
                           // but its not. So lets trigger its validation as well.
                           scope.$broadcast('schemaFormValidate');
@@ -1553,6 +1481,81 @@ angular.module('schemaForm').provider('schemaForm',
 
 }]);
 
+/**
+ * @ngdoc service
+ * @name sfSelect
+ * @kind function
+ *
+ */
+angular.module('schemaForm').factory('sfSelect', ['sfPath', function(sfPath) {
+  var numRe = /^\d+$/;
+
+  /**
+    * @description
+    * Utility method to access deep properties without
+    * throwing errors when things are not defined.
+    * Can also set a value in a deep structure, creating objects when missing
+    * ex.
+    * var foo = Select('address.contact.name',obj)
+    * Select('address.contact.name',obj,'Leeroy')
+    *
+    * @param {string} projection A dot path to the property you want to get/set
+    * @param {object} obj   (optional) The object to project on, defaults to 'this'
+    * @param {Any}    valueToSet (opional)  The value to set, if parts of the path of
+    *                 the projection is missing empty objects will be created.
+    * @returns {Any|undefined} returns the value at the end of the projection path
+    *                          or undefined if there is none.
+    */
+  return function(projection, obj, valueToSet) {
+    if (!obj) {
+      obj = this;
+    }
+    //Support [] array syntax
+    var parts = typeof projection === 'string' ? sfPath.parse(projection) : projection;
+
+    if (typeof valueToSet !== 'undefined' && parts.length === 1) {
+      //special case, just setting one variable
+      obj[parts[0]] = valueToSet;
+      return obj;
+    }
+
+    if (typeof valueToSet !== 'undefined' &&
+        typeof obj[parts[0]] === 'undefined') {
+       // We need to look ahead to check if array is appropriate
+      obj[parts[0]] = parts.length > 2 && numRe.test(parts[1]) ? [] : {};
+    }
+
+    var value = obj[parts[0]];
+    for (var i = 1; i < parts.length; i++) {
+      // Special case: We allow JSON Form syntax for arrays using empty brackets
+      // These will of course not work here so we exit if they are found.
+      if (parts[i] === '') {
+        return undefined;
+      }
+      if (typeof valueToSet !== 'undefined') {
+        if (i === parts.length - 1) {
+          //last step. Let's set the value
+          value[parts[i]] = valueToSet;
+          return valueToSet;
+        } else {
+          // Make sure to create new objects on the way if they are not there.
+          // We need to look ahead to check if array is appropriate
+          var tmp = value[parts[i]];
+          if (typeof tmp === 'undefined' || tmp === null) {
+            tmp = numRe.test(parts[i + 1]) ? [] : {};
+            value[parts[i]] = tmp;
+          }
+          value = tmp;
+        }
+      } else if (value) {
+        //Just get nex value.
+        value = value[parts[i]];
+      }
+    }
+    return value;
+  };
+}]);
+
 /*  Common code for validating a value against its form and schema definition */
 /* global tv4 */
 angular.module('schemaForm').factory('sfValidator', [function() {
@@ -2109,6 +2112,9 @@ angular.module('schemaForm').directive('sfField',
                     scope.ngModel.$setValidity(error, validity === true);
 
                     if (validity === true) {
+                      // Re-trigger model validator, that model itself would be re-validated
+                      scope.ngModel.$validate();
+
                       // Setting or removing a validity can change the field to believe its valid
                       // but its not. So lets trigger its validation as well.
                       scope.$broadcast('schemaFormValidate');
@@ -2677,7 +2683,7 @@ angular.module('schemaForm')
         // part of the form or schema is chnaged without it being a new instance.
         scope.$on('schemaFormRedraw', function() {
           var schema = scope.schema;
-          var form   = scope.initialForm || ['*'];
+          var form   = scope.initialForm ? angular.copy(scope.initialForm) : ['*'];
           if (schema) {
             render(schema, form);
           }
@@ -2817,7 +2823,13 @@ angular.module('schemaForm').directive('schemaValidate', ['sfValidator', '$parse
         var schema = form.schema;
 
         // A bit ugly but useful.
-        scope.validateField =  function() {
+        scope.validateField =  function(formName) {
+          
+          // If we have specified a form name, and this model is not within 
+          // that form, then leave things be.
+          if(formName != undefined && ngModel.$$parentForm.$name !== formName) {
+            return;
+          }
 
           // Special case: arrays
           // TODO: Can this be generalized in a way that works consistently?
@@ -2864,7 +2876,9 @@ angular.module('schemaForm').directive('schemaValidate', ['sfValidator', '$parse
         });
 
         // Listen to an event so we can validate the input on request
-        scope.$on('schemaFormValidate', scope.validateField);
+        scope.$on('schemaFormValidate', function(event, formName) {
+          scope.validateField(formName);
+        });
 
         scope.schemaError = function() {
           return error;
--- gulp/tasks/minify.js
+++ gulp/tasks/minify.js
@@ -1,30 +1,30 @@
-var gulp = require('gulp'),
-  concat = require('gulp-concat'),
-  rename = require('gulp-rename'),
-  umd    = require('gulp-umd'),
-  uglify = require('gulp-uglify');
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+var rename = require('gulp-rename');
+var umd    = require('gulp-umd');
+var uglify = require('gulp-uglify');
 
 gulp.task('minify', function() {
   gulp.src([
-    './src/module.js',
-    './src/sfPath.js',
-    './src/services/*.js',
-    './src/directives/*.js'
+      './src/module.js',
+      './src/sfPath.js',
+      './src/services/*.js',
+      './src/directives/*.js'
     ])
-  .pipe(concat('schema-form.js'))
-  .pipe(umd({
-    dependencies: function() {
-      return [
-        {name: 'angular'},
-        {name: 'objectpath'},
-        {name: 'tv4'},
-      ];
-    },
-    exports: function() {return 'schemaForm';},
-    namespace: function() {return 'schemaForm';}
+    .pipe(concat('schema-form.js'))
+    .pipe(umd({
+      dependencies: function() {
+        return [
+          {name: 'angular'},
+          {name: 'objectpath'},
+          {name: 'tv4'},
+        ];
+      },
+      exports: function() {return 'schemaForm';},
+      namespace: function() {return 'schemaForm';}
     }))
-  .pipe(gulp.dest('./dist/'))
-  .pipe(uglify())
-  .pipe(rename('schema-form.min.js'))
-  .pipe(gulp.dest('./dist/'));
+    .pipe(gulp.dest('./dist/'))
+    .pipe(uglify())
+    .pipe(rename('schema-form.min.js'))
+    .pipe(gulp.dest('./dist/'));
 });
