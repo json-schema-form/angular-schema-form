3c48c4f1ae5733602d0d0472bfa15518f7e7f98f
Kyse <kyse@kyse.us>
2016-05-30 21:48:22 -0400

    sfNewArray: Change titleMapValue binding to prevent ngModel target array being cleared.  Allows multiple checkboxes (array builder) controls to target the same ngModel target array.

--- src/directives/newArray.js
+++ src/directives/newArray.js
@@ -120,15 +120,12 @@ function(sel, sfPath, schemaForm) {
             if (vals && vals !== old) {
               var arr = getOrCreateModel();
 
-              // Apparently the fastest way to clear an array, readable too.
-              // http://jsperf.com/array-destroy/32
-              while (arr.length > 0) {
-                arr.pop();
-              }
-              form.titleMap.forEach(function(item, index) {
-                if (vals[index]) {
+              form.titleMap.forEach(function (item, index) {
+                var arrIndex = arr.indexOf(item.value);
+                if (arrIndex === -1 && vals[index])
                   arr.push(item.value);
-                }
+                if (arrIndex !== -1 && !vals[index])
+                  arr.splice(arrIndex, 1);
               });
 
               // Time to validate the rebuilt array.
