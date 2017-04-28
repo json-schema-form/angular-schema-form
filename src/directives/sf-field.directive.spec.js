0a2075518891ee97713adf5428cc67d94cbe2bd2
Mina Nagy Zaki <mnzaki@gmail.com>
2017-04-28 10:46:14 +0200

    tests: sf-field tests for form.key generation
    issue json-schema-form/angular-schema-form#870

---
+++ src/directives/sf-field.directive.spec.js
@@ -0,0 +1,80 @@
+chai.should();
+
+var runSync = function (scope, tmpl) {
+  var directiveScope = tmpl.isolateScope();
+  var stub = sinon.stub(directiveScope, 'resolveReferences', function(schema, form) {
+    directiveScope.render(schema, form);
+  });
+  scope.$apply();
+}
+
+describe('sf-field.directive.js',function() {
+  beforeEach(module('schemaForm'));
+  beforeEach(
+    module(function($sceProvider){
+      $sceProvider.enabled(false);
+    })
+  );
+
+  var keyTests = [
+    {
+      name: 'array of objects',
+      targetKey: ['arrayOfObjects', 0, 'stringVal'],
+      schema: {
+        type: 'object',
+        properties: {
+          arrayOfObjects: {
+            type: 'array',
+            items: {
+              type: 'object',
+              properties: {
+                stringVal: {
+                  type: 'string',
+                  'x-schema-form': {
+                    htmlClass: 'targetKey'
+                  }
+                }
+              }
+            }
+          }
+        }
+      },
+    },
+
+    {
+      name: 'array of strings',
+      targetKey: ['arrayOfStrings', 0],
+      schema: {
+        type: 'object',
+        properties: {
+          arrayOfStrings: {
+            type: 'array',
+            items: {
+              type: 'string',
+              'x-schema-form': {
+                htmlClass: 'targetKey'
+              }
+            }
+          }
+        }
+      }
+    }
+  ];
+
+  keyTests.forEach(function(keyTest) {
+    it('should generate correct form keys for ' + keyTest.name, function(done) {
+      inject(function($compile,$rootScope) {
+        var scope = $rootScope.$new();
+        scope.model = {};
+        scope.schema = keyTest.schema;
+
+        var tmpl = angular.element('<form sf-schema="schema" sf-model="model"></form>');
+
+        $compile(tmpl)(scope);
+        runSync(scope, tmpl);
+
+        tmpl.children().find('.targetKey').scope().form.key.should.deep.equal(keyTest.targetKey);
+      });
+    });
+  });
+});
