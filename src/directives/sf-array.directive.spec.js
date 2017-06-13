7906ac66bbbda870f0e36a780db92ecc8c8f6287
Mina Nagy Zaki <mnzaki@gmail.com>
2017-06-13 22:42:25 +0200

    add sf-array deletion failing tests
    related to json-schema-form/angular-schema-form#870 and
    json-schema-form/angular-schema-form#874

--- src/directives/sf-array.directive.spec.js
+++ src/directives/sf-array.directive.spec.js
@@ -22,9 +22,14 @@ describe('sf-array.directive.js', function() {
             "type": "array",
             "description": "foobar",
             "items": {
-              "title": "Name",
-              "type": "string",
-              "default": 6
+              "type": "object",
+              "properties": {
+                "name": {
+                  "title": "Name",
+                  "type": "string",
+                  "default": 6
+                }
+              }
             }
           }
         }
@@ -61,6 +66,37 @@ describe('sf-array.directive.js', function() {
       }, 0)
       //tmpl.$valid.should.be.true;
 
+    });
+  });
+
+  it('should not delete innocent items on delete', function(done) {
+
+    tmpl = angular.element('<form name="testform" sf-schema="schema" sf-form="form" sf-model="model" json="{{model | json}}"></form>');
+
+    inject(function($compile, $rootScope) {
+      var scope = $rootScope.$new();
+      scope.model = {names: [{name: "0"}, {name: "1"}, {name: "2"}, {name: "3"}]};
+
+      scope.schema = exampleSchema;
+
+      scope.form = ["*"];
+
+      $compile(tmpl)(scope);
+      runSync(scope, tmpl);
+
+      tmpl.find('div.help-block').text().should.equal('foobar');
+
+      var close = tmpl.find('button.close');
+      close.eq(1).click();
+
+      $rootScope.$apply();
+
+      setTimeout(function() {
+        scope.model.names[0].name.should.equal("0");
+        scope.model.names[1].name.should.equal("2");
+        scope.model.names[2].name.should.equal("3");
+        done();
+      }, 0)
     });
   });
 });
